d2efb6756504ffcf2fd3d0c08bb30af632516c7c
vijay shukla <vijay.shukla@eduvanz.in>
2019-01-29 11:18:04 +0530

    Vijay

---
+++ app/src/main/java/com/eduvanzapplication/newUI/pojo/AddressSameAsCurrentPOJO.java
@@ -0,0 +1,11 @@
+package com.eduvanzapplication.newUI.pojo;
+
+/**
+ * Created by Vijay on 29/7/17.
+ */
+
+public class AddressSameAsCurrentPOJO {
+
+    public String address;
+    public String id;
+}
