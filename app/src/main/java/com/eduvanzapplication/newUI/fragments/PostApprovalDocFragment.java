2b5583da58896f098af0b5a47a6a0773612feac9
vijay shukla <vijay.shukla@eduvanz.in>
2019-03-23 19:44:08 +0530

    ui2 update

--- app/src/main/java/com/eduvanzapplication/newUI/fragments/PostApprovalDocFragment.java
+++ app/src/main/java/com/eduvanzapplication/newUI/fragments/PostApprovalDocFragment.java
@@ -1,5 +1,6 @@
 package com.eduvanzapplication.newUI.fragments;
 
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.eduvanzapplication.R;
+
+import static com.airbnb.lottie.model.layer.Layer.LayerType.Text;
+import static com.eduvanzapplication.R.*;
+import static com.eduvanzapplication.R.color.red;
 
 
 public class PostApprovalDocFragment extends Fragment {
@@ -33,13 +38,16 @@ public class PostApprovalDocFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_postapprovaldoc, container, false);
-        txtProcessingFee = view.findViewById(R.id.txtProcessingFee);
+        view = inflater.inflate(layout.fragment_postapprovaldoc, container, false);
+        txtProcessingFee = view.findViewById(id.txtProcessingFee);
 
-        SpannableString string = new SpannableString("Text with\nBullet point");
-        string.setSpan(new BulletSpan(40, 20), 10, 22, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        SpannableString string = new SpannableString("\u25CF Processing Fee");
+//        string.setSpan(new BulletSpan(40, 20), 10, 22, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-        txtProcessingFee.setText(string);
+//        SpannableString string1 = new SpannableString("Text with\nBullet point");
+//        string1.setSpan(new BulletSpan(40, red, 20), 10, 22, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+//        txtProcessingFee.setText(string);
         return view;
 
     }
